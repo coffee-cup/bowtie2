68bdbe0501eb26474d1e98a0c09b8705dd503433
Jake Runzer <jakerunzer@gmail.com>
2020-11-14 22:36:35 -0700

    Initial Commit

---
+++ bowtie2/bowtie2App.swift
@@ -0,0 +1,20 @@
+//
+//  bowtie2App.swift
+//  bowtie2
+//
+//  Created by Jake Runzer on 2020-11-14.
+//
+
+import SwiftUI
+
+@main
+struct bowtie2App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
