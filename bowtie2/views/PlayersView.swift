6b29cb76bf25ace6fe3be980b81ee523f52b0bf4
Jake Runzer <jakerunzer@gmail.com>
2020-11-15 13:26:56 -0700

    delete players

--- bowtie2/views/PlayersView.swift
+++ bowtie2/views/PlayersView.swift
@@ -23,10 +23,18 @@ struct PlayersView: View {
     
     var body: some View {
         NavigationView {
+        
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(players, id: \.self) { player in
                         PlayerCard(name: player.name!, colour: player.colour!)
+                            .contextMenu {
+                                Button(action: {
+                                    self.deletePlayer(player: player)
+                                }) {
+                                    Text("Delete Player")
+                                }
+                            }
                     }
                 }
                 .padding(35)
@@ -44,6 +52,27 @@ struct PlayersView: View {
                 }
             }
         }
+    }
+    
+    private func deletePlayer(player: Player) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7){
+            viewContext.delete(player)
+            
+            try! viewContext.save()
+        }
+        
+//        withAnimation {
+//            do {
+//                offsets.map { players[$0] }.forEach(viewContext.delete)
+//
+//                try viewContext.save()
+//            } catch {
+//                // Replace this implementation with code to handle the error appropriately.
+//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//                let nsError = error as NSError
+//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//            }
+//        }
     }
 }
 
