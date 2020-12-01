52707f5319d7c01303b459be972dceabe5347d88
Jake Runzer <jakerunzer@gmail.com>
2020-12-01 00:59:44 -0700

    add app icon

--- bowtie2/Colours.swift
+++ bowtie2/Colours.swift
@@ -7,4 +7,5 @@
 
 import SwiftUI
 
-let primaryGradient = Gradient(colors: [ Color(hex: "#ff6a00"), Color(hex: "#ee0979")])
+//let primaryGradient = Gradient(colors: [ Color(hex: "#ff6a00"), Color(hex: "#ee0979")])
+let primaryGradient = Gradient(colors: [ Color(hex: "#FFA07A"), Color(hex: "#FF1493")])
