9db7ab5320514880f9f8d1239d278f797fe3cef2
Jake Runzer <jakerunzer@gmail.com>
2020-12-14 00:12:30 -0700

    update bounding boxes of calc buttons

--- bowtie2/views/EnterScoreView.swift
+++ bowtie2/views/EnterScoreView.swift
@@ -18,10 +18,9 @@ struct CalcButton: View {
             Text(text)
                 .foregroundColor(.primary)
                 .font(.system(size: 22))
-                .padding(.vertical, 22)
+                .padding(.vertical, 28)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
@@ -69,7 +68,7 @@ struct EnterScoreView: View {
     let addScore: ((_ playerScore: PlayerScore, _ score: Int) -> ())?
     
     var columns: [GridItem] =
-        Array(repeating: .init(.flexible()), count: 3)
+        Array(repeating: .init(.flexible(), spacing: 0), count: 3)
     
     var body: some View {
         ModalView {
@@ -80,35 +79,41 @@ struct EnterScoreView: View {
                 
                 ScoreView(score: score, isNegative: $isNegative)
                 
-                LazyVGrid(columns: columns) {
-                    CalcButton(text: "1", onTap: { self.addValue(digit: 1)})
-                    CalcButton(text: "2", onTap: { self.addValue(digit: 2)})
-                    CalcButton(text: "3", onTap: { self.addValue(digit: 3)})
-                    CalcButton(text: "4", onTap: { self.addValue(digit: 4)})
-                    CalcButton(text: "5", onTap: { self.addValue(digit: 5)})
-                    CalcButton(text: "6", onTap: { self.addValue(digit: 6)})
-                    CalcButton(text: "7", onTap: { self.addValue(digit: 7)})
-                    CalcButton(text: "8", onTap: { self.addValue(digit: 8)})
-                    CalcButton(text: "9", onTap: { self.addValue(digit: 9)})
-                }
-                LazyVGrid(columns: columns) {
-                    CalcButton(text: "C", onTap: { self.clearScore() })
-                    CalcButton(text: "0", onTap: { self.addValue(digit: 0)})
-                    Button(action: {
-                        if let addScore = self.addScore {
-                            addScore(playerScore, (isNegative ? -1 : 1) * score)
-                        }
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        Text("Go")
-                            .foregroundColor(.white).bold()
+                VStack(spacing: 0) {
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        CalcButton(text: "1", onTap: { self.addValue(digit: 1)})
+                        CalcButton(text: "2", onTap: { self.addValue(digit: 2)})
+                        CalcButton(text: "3", onTap: { self.addValue(digit: 3)})
+                        CalcButton(text: "4", onTap: { self.addValue(digit: 4)})
+                        CalcButton(text: "5", onTap: { self.addValue(digit: 5)})
+                        CalcButton(text: "6", onTap: { self.addValue(digit: 6)})
+                        CalcButton(text: "7", onTap: { self.addValue(digit: 7)})
+                        CalcButton(text: "8", onTap: { self.addValue(digit: 8)})
+                        CalcButton(text: "9", onTap: { self.addValue(digit: 9)})
                     }
-                    .frame(maxWidth: 44, maxHeight: 44)
-                    .padding(.all)
-                    .background(settings.theme.gradient)
-                    .cornerRadius(44)
+                    LazyVGrid(columns: columns, spacing: 0) {
+                        CalcButton(text: "C", onTap: { self.clearScore() })
+                        CalcButton(text: "0", onTap: { self.addValue(digit: 0)})
+                        
+                        Button(action: {
+                            if let addScore = self.addScore {
+                                addScore(playerScore, (isNegative ? -1 : 1) * score)
+                            }
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            HStack {
+                            Text("Go")
+                                .foregroundColor(.white).bold()
+                                .frame(maxWidth: 44, maxHeight: 44)
+                                .padding(.all)
+                                .background(settings.theme.gradient)
+                                .cornerRadius(44)
+                            }
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+                    }
+                    .padding(.bottom)
                 }
-                .padding(.bottom)
                 
                 Spacer(minLength: 20)
             }
