0642c574432f33479609da80c7b0c147b1ba0953
Jake Runzer <jakerunzer@gmail.com>
2020-12-29 14:38:08 -0700

    persistent container merge policy

--- bowtie2/Persistence.swift
+++ bowtie2/Persistence.swift
@@ -62,9 +62,11 @@ struct PersistenceController {
 
     init(inMemory: Bool = false) {
         container = NSPersistentCloudKitContainer(name: "bowtie2")
+        
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
+        
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -81,5 +83,8 @@ struct PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
     }
 }
